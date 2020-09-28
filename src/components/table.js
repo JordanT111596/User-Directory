b4c4a6c9ffa20f27a866c82d97994d2e7ee49425
Jordan Triplett <JordanT111596@gmail.com>
2020-09-28 11:23:03 -0400

    Turned table function into class

--- src/components/table.js
+++ src/components/table.js
@@ -2,36 +2,42 @@ import React from 'react';
 import Users from "./users.js";
 import users from "../users.json";
 
-function Table() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-10">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                {/* Use these onClicks for sorting later */}
-                                <th scope="col" onClick={clickTest}>#</th>
-                                <th scope="col" onClick={clickTest}>First</th>
-                                <th scope="col" onClick={clickTest}>Last</th>
-                                <th scope="col" onClick={clickTest}>Email</th>
-                                <th scope="col" onClick={clickTest}>Phone</th>
-                            </tr>
-                        </thead>
-                        {users.map(user => (
-                            <Users
-                                key={user.id}
-                                id={user.id.toString()}
-                                firstName={user.firstName}
-                                lastName={user.lastName}
-                                email={user.email}
-                                phone={user.phone} />
-                        ))}
-                    </table>
+class Table extends React.Component {
+    state = {
+        users: users
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-10">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    {/* Use these onClicks for sorting later */}
+                                    <th scope="col" onClick={clickTest}>#</th>
+                                    <th scope="col" onClick={clickTest}>First</th>
+                                    <th scope="col" onClick={clickTest}>Last</th>
+                                    <th scope="col" onClick={clickTest}>Email</th>
+                                    <th scope="col" onClick={clickTest}>Phone</th>
+                                </tr>
+                            </thead>
+                            {this.state.users.map(user => (
+                                <Users
+                                    key={user.id}
+                                    id={user.id.toString()}
+                                    firstName={user.firstName}
+                                    lastName={user.lastName}
+                                    email={user.email}
+                                    phone={user.phone} />
+                            ))}
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 function clickTest() {
